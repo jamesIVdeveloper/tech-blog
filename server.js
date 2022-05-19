9a26d3e913ab0c173bf47462a25e25828740b542
James Murphy <jamesivdeveloper@gmail.com>
2022-05-19 16:57:08 -0500

    add CRUD operations for users

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const routes = require("./routes");
 const sequelize = require("./config/connection");
 
 const app = express();
@@ -6,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on Port: ${PORT}`));
