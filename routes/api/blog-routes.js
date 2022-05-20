b6331cf48233cb225efd9badbb06b71dbe5eda0c
James Murphy <jamesivdeveloper@gmail.com>
2022-05-20 12:45:51 -0500

    add CRUD routes for blog

---
+++ routes/api/blog-routes.js
@@ -0,0 +1,104 @@
+const router = require("express").Router();
+const { Blog, User } = require("../../models");
+
+router.get("/", (req, res) => {
+  Blog.findAll({
+    attributes: ["id", "title", "content", "created_at"],
+    order: [["created_at", "DESC"]],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => res.json(dbBlogData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Blog.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  Blog.create({
+    title: req.body.title,
+    content: req.body.content,
+    user_id: req.body.user_id,
+  })
+    .then((dbBlogData) => res.json(dbBlogData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Blog.update(
+    {
+      title: req.body.title,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Blog.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+      res.json(dbBlogData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
