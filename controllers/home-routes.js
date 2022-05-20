e715d84f6e045d2abac92b32fe6578a3e9609e64
James Murphy <jamesivdeveloper@gmail.com>
2022-05-20 16:13:08 -0500

    setup handlebars and add homepage template

---
+++ controllers/home-routes.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Blog, User, Comment } = require("../models");
+
+router.get("/", (req, res) => {
+  Blog.findAll({
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
+      res.render("homepage", { blogs });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
