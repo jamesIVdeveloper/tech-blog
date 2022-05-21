f3d4fc3d1d9ccaa2cc21a3113d7af5f06bb33b0b
James Murphy <jamesivdeveloper@gmail.com>
2022-05-21 01:16:28 -0500

    add authentication for users with login and logout capability

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,8 @@ const sequelize = require("../config/connection");
 const { Blog, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
+
   Blog.findAll({
     attributes: ["id", "content", "title", "created_at"],
     include: [
@@ -28,6 +30,15 @@ router.get("/", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require("express-session");
+
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +27,7 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
 app.use(routes);
 
