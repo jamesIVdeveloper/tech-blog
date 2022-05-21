b83e15ca5680286acb7903d0565a366071cb32fe
James Murphy <jamesivdeveloper@gmail.com>
2022-05-21 14:46:22 -0500

    add single blog handlebar and manage rendering based on logged in status

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -24,7 +24,7 @@ router.get("/", (req, res) => {
   })
     .then((dbBlogData) => {
       const blogs = dbBlogData.map((blog) => blog.get({ plain: true }));
-      res.render("homepage", { blogs });
+      res.render("homepage", { blogs, loggedIn: req.session.loggedIn });
     })
     .catch((err) => {
       console.log(err);
@@ -39,6 +39,43 @@ router.get("/login", (req, res) => {
   }
 
   res.render("login");
+});
+
+router.get("/blog/:id", (req, res) => {
+  Blog.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbBlogData) => {
+      if (!dbBlogData) {
+        res.status(404).json({ message: "No blog found with this id" });
+        return;
+      }
+
+      const blog = dbBlogData.get({ plain: true });
+
+      res.render("single-blog", { blog, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
