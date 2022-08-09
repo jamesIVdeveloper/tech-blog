849140385111b63e69d55c6326ab16d5cf82982f
James Murphy <jamesivdeveloper@gmail.com>
2022-08-08 22:09:49 -0500

    revert

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,7 +18,6 @@ async function signupFormHandler(event) {
 
     if (response.ok) {
       console.log("success");
-      document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
     }
